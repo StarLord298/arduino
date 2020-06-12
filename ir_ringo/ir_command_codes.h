4d1b9d079db753e7df15e3dd7992f0286b21abce
StarLord298 <4reient4@gmail.com>
2020-06-12 09:08:31 +0300

    Update ir_command_codes.h

--- ir_ringo/ir_command_codes.h
+++ ir_ringo/ir_command_codes.h
@@ -2,17 +2,17 @@
 //     You can type codes from several IR remotes.
 // RU: Коды команд, принимаемых от ИК-пульта. Одна команда может вызываться несколькими
 //     кодами. Вы можете ввести коды от нескольких ИК-пультов.
-const long IR_COMMAND_FORWARD_CODES[] = { 527175, 2050, 2 };
-const long IR_COMMAND_BACKWARD_CODES[] = { 920391, 2056, 8 };
-const long IR_COMMAND_TURN_LEFT_CODES[] = { 789319, 2052, 4 };
-const long IR_COMMAND_TURN_RIGHT_CODES[] = { 658247, 2054, 6 };
-const long IR_COMMAND_FORWARD_LEFT_CODES[] = { 2887, 2049, 1 };
-const long IR_COMMAND_FORWARD_RIGHT_CODES[] = { 265031, 2051, 3 };
-const long IR_COMMAND_BACKWARD_LEFT_CODES[] = { 68423, 2057, 9 };
-const long IR_COMMAND_BACKWARD_RIGHT_CODES[] = { 396103, 2055, 7 };
-const long IR_COMMAND_STOP_CODES[] = { 133959, 2053, 5 };
-const long IR_COMMAND_VERY_SLOW_CODES[] = { 420679, 2088, 40 };
-const long IR_COMMAND_SLOW_CODES[] = { 617287, 2091, 43 };
-const long IR_COMMAND_FAST_CODES[] = { 92999, 2074, 26 };
-const long IR_COMMAND_VERY_FAST_CODES[] = { 944967, 2075, 27 };
+const long IR_COMMAND_FORWARD_CODES[] = { 527175, 0xFF18E7, 2 };
+const long IR_COMMAND_BACKWARD_CODES[] = { 920391, 0xFF4AB5, 8 };
+const long IR_COMMAND_TURN_LEFT_CODES[] = { 789319, 0xFF10EF, 4 };
+const long IR_COMMAND_TURN_RIGHT_CODES[] = { 658247, 0xFF5AA5, 6 };
+const long IR_COMMAND_FORWARD_LEFT_CODES[] = { 2887, 0xFFE01F, 1 };
+const long IR_COMMAND_FORWARD_RIGHT_CODES[] = { 265031, 0xFFA857, 3 };
+const long IR_COMMAND_BACKWARD_LEFT_CODES[] = { 68423, 0xFF02FD, 9 };
+const long IR_COMMAND_BACKWARD_RIGHT_CODES[] = { 396103, 0xFFC23D , 7 };
+const long IR_COMMAND_STOP_CODES[] = { 133959, 0xFF38C7, 5 };
+const long IR_COMMAND_VERY_SLOW_CODES[] = { 420679, 0xFFA25D, 40 };
+const long IR_COMMAND_SLOW_CODES[] = { 617287, 0xFF629D, 43 };
+const long IR_COMMAND_FAST_CODES[] = { 92999, 0xFFE21D, 26 };
+const long IR_COMMAND_VERY_FAST_CODES[] = { 944967, 0xFF22DD , 27 };
 
